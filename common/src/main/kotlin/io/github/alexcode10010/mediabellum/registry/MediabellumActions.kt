d8f6df302bc605b4e7f90e9df3fced00a16518a2
AlexCode10010 <alexsuper9999@gmail.com>
2025-09-04 10:50:05 +0200

    Update MediabellumActions.kt

--- common/src/main/kotlin/io/github/alexcode10010/mediabellum/registry/MediabellumActions.kt
+++ common/src/main/kotlin/io/github/alexcode10010/mediabellum/registry/MediabellumActions.kt
@@ -12,7 +12,7 @@ object MediabellumActions : MediabellumRegistrar<ActionRegistryEntry>(
     HexRegistries.ACTION,
     { HexActions.REGISTRY },
 ) {
-    val CONGRATULATE = make("congratulate", HexDir.WEST, "eed", OpCongratulate)
+    val CONGRATULATE = make("congratulate", HexDir.NORTHWEST, "eeeeedded", OpCongratulate)
 
     private fun make(name: String, startDir: HexDir, signature: String, action: Action) =
         make(name, startDir, signature) { action }
